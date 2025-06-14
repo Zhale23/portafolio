2973bd85bf3f718c16559fc73968032f072ea802
Zharick <158623663+Zhale23@users.noreply.github.com>
2025-06-14 14:37:21 -0500

    elimine carpetas

--- plantilla/script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById('mode-toggle');
-const body = document.body;
-
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  const isDarkMode = body.classList.contains('dark-mode');
-  toggleButton.textContent = isDarkMode ? '☀️' : '🌙';
-});
